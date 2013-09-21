f2d6027c81846eb78e75cf0ed5ef312f0111d2d7
flar2 <asegaert@gmail.com>
2013-09-20 23:00:13 -0400

    freezer: add new freezable helpers using freezer_do_not_count

--- include/linux/freezer.h
+++ include/linux/freezer.h
@@ -161,7 +161,17 @@ static inline int freezer_should_skip(struct task_struct *p)
 	__retval;							\
 })
 
-#else /* !CONFIG_FREEZER */
+#define wait_event_freezable_exclusive(wq, condition)		\
+({									\
+	int __retval;							\
+	freezer_do_not_count();						\
+	__retval = wait_event_interruptible_exclusive(wq, condition);	\
+	freezer_count();						\
+	__retval;							\
+})
+
+
+#else 
 static inline bool frozen(struct task_struct *p) { return false; }
 static inline bool freezing(struct task_struct *p) { return false; }
 static inline void __thaw_task(struct task_struct *t) {}
